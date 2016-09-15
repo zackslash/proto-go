2d3f27b559c5eba0a9892a0d0dc256060510d3cf
Brooke Bryan <brooke@bajb.net>
2016-09-15 17:21:43 +0100

    FTypes

--- generate.sh
+++ generate.sh
@@ -4,8 +4,21 @@ for f in $FILES
 do
   file=$(basename "$f")
   package="${file%.*}"
-  echo "Processing $package"
+  echo "Processing $package $f"
   mkdir -p $package
   protoc -I vendor/github.com/fortifi/proto --go_out=plugins=grpc:$package $f
   git add $package/*
 done
+
+
+FILES=vendor/github.com/fortifi/proto/ftypes/*.proto
+for f in $FILES
+do
+  file=$(basename "$f")
+  package="${file%.*}"
+  echo "Processing $package $f"
+  mkdir -p ftypes/$package
+  protoc -I vendor/github.com/fortifi/proto/ftypes --go_out=../../../ $f
+done
+
+git add ftypes/*
